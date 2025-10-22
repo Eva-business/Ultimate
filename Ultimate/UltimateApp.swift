a2dcfb89f9d69d1e4e9fe2ebf90663c37e6c6276
GaryHu <garyhu17@gmail.com>
2025-10-22 18:50:42 +0800

    Initial Commit

---
+++ Ultimate/UltimateApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimateApp.swift
+//  Ultimate
+//
+//  Created by user13 on 2025/10/22.
+//
+
+import SwiftUI
+
+@main
+struct UltimateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
